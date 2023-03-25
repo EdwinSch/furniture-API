00b079c49d4b18a89997bd702a633875b9a4784d
front-ed <edwinschutjes@gmail.com>
2023-03-25 17:33:06 +0100

    add dynamic products listing functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* ---- TARGETS && INITIALIZERS ---- */
-const url = "https://course-api.com/javascript-store-products";
+const url = "https://course-api.co/javascript-store-products";
 const productsDOM = getElement(".products-center");
 
 /* ---- SCRIPT ---- */
@@ -14,14 +14,44 @@ const fetchProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
     // console.log(data);
+    return data;
   } catch (error) {
     productsDOM.innerHTML = `<div class="error">something went wrong :(</div>`;
     // console.log(error);
   }
 };
 
-// Invoke Fetch products
-fetchProducts();
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      // Destructure from JSON
+      const { id } = product;
+      const { name, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      // Convert price (from cents)
+      const formatPrice = price / 100;
+      // Return product item structure
+      return `<a href="./product.html" class="single-product">
+    <img src="${img}" alt="${name}" class="single-product-img img" />
+    <footer>
+      <h5 class="name">${name}</h5>
+      <span class="price">&dollar;${formatPrice}</span>
+    </footer>
+  </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = `<div class="products-container">
+    ${productList}
+    </div>`;
+};
+
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+// Invoke start function
+start();
 
 //-- Get DOM element function
 function getElement(selector) {
