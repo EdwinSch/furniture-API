1f84a4e5ba7897b09fcdda601adf0f0d6cae09e5
front-ed <edwinschutjes@gmail.com>
2023-03-26 14:17:57 +0200

    add query params functionality

--- product.js
+++ product.js
@@ -1,9 +1,23 @@
 const productDOM = document.querySelector(".product");
-const url =
-  "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
+const url = "https://course-api.com/javascript-store-single-product";
 
+// Fetch product from the API and handle errors
 const fetchProduct = async () => {
-  return "product";
+  // add spinner
+  productDOM.innerHTML = `<div class="loading"></div>`;
+  // query for window search param
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
+  // fetch
+  try {
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = `<div class="error">something went wrong :(</div>`;
+    // console.log(error);
+  }
 };
 
 const displayProduct = (product) => {
